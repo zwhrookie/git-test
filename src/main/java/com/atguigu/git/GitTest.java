6f9ab6bf7051a2debbd313423242780682b425bd
zwhrookie <112365137+zwhrookie@users.noreply.github.com>
2022-10-16 16:59:43 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix commit");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
