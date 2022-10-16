d0d2e91edb9a51fbc3579cdfaf6a1cb827527061
zwh <zwh152@qq.com>
2022-10-16 16:45:34 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git!444");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push test");
 
     }
 }
